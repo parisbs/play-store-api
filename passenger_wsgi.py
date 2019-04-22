fc06358e5f97df8c9273e216a6290417d5f22c14
Paris N. Baltazar Salguero <32491290+parisbs@users.noreply.github.com>
2019-04-22 18:02:32 -0500

    Change entri point to support cgi servers

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,8 @@
+#!/home/blindoap/virtualenv/play-store-api/3.6/bin/python
+
+from flup.server.fcgi import WSGIServer
 from play_store_api import create_api
 
-
-api = create_api()
-
-if __name__ == "__main__":
-    api.run()
+if __name__ == '__main__':
+    api = create_api()
+    WSGIServer(api).run()
